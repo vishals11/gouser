5fe7f08508026522e9e223ec7aae1f492e5c0209
Vishal Sanap <vishalsanap20@gmail.com>
2020-06-18 16:41:45 +0530

    Added config and main package

---
+++ config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"log"
+	"os"
+
+	"gopkg.in/go-playground/validator.v9"
+)
+
+// Config contains project configuration
+type Config struct {
+	Port string `validate:"required"`
+
+	DBHost     string `validate:"required"`
+	DBUserName string `validate:"required"`
+	DBPassword string `validate:"required"`
+	DBPort     string `validate:"required"`
+	DBName     string `validate:"required"`
+}
+
+var config Config
+
+func init() {
+	config.Port = ":" + os.Getenv("PORT")
+	config.DBHost = os.Getenv("DB_HOST")
+	config.DBUserName = os.Getenv("DB_USERNAME")
+	config.DBPassword = os.Getenv("DB_PASSWORD")
+	config.DBPort = os.Getenv("DB_PORT")
+	config.DBName = os.Getenv("DB_NAME")
+
+	validate := validator.New()
+	err := validate.Struct(config)
+	if err != nil {
+		log.Fatalf("Environment Variables not set:\n%s\n", err)
+	}
+}
+
+// Get configuration structure
+func Get() Config {
+	return config
+}
---
+++ main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/vishals11/gouser/config"
+)
+
+func main() {
+	cfg := config.Get()
+
+	fmt.Printf("Server listening on port: %s\n", cfg.Port)
+	log.Fatal(http.ListenAndServe(cfg.Port, r))
+}
